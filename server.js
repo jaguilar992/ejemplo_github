ad128ffd7463ba430bbd539450d4527477bc14bf
jaguilar992 <jaguilar992@gmail.com>
2022-10-27 16:55:06 -0600

    Feature: Endpoint saludo

--- server.js
+++ server.js
@@ -2,8 +2,14 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+app.get("/saludo", (req, res) => {
+  res.send("Hola");
+})
+
 app.listen(PORT, () => {
   console.log(
     `PID: ${process.pid}. Servidor express escuchando en puerto ${PORT}`
   );
 });
+
+
